162c5f04a39a89564de726b0a417239deb825667
kyle o'neill <kyleoneill24@gmail.com>
2023-02-24 18:22:54 -0500

    Refactor, move logic out of UI

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod planet;
-use planet::{Planet, TimeUnit};
+use planet::{Planet, TimeUnit, OrbitalMechanics};
 use eframe::{egui, Frame};
 use egui::{Context, InnerResponse, Ui, Vec2, vec2};
 use std::default::Default;
@@ -9,8 +9,6 @@ use eframe::epaint::Color32;
 use egui::RichText;
 
 fn main() -> Result<(), eframe::Error> {
-    let planets = Planet::get_all_planets();
-
     tracing_subscriber::fmt::init();
 
     let options = eframe::NativeOptions {
@@ -20,12 +18,12 @@ fn main() -> Result<(), eframe::Error> {
     eframe::run_native(
         "Orbital Windows",
         options,
-        Box::new(|cc| Box::new(MyApp::new(planets)))
+        Box::new(|cc| Box::new(MyApp::new()))
     )
 }
 
 struct MyApp {
-    planets: [Planet; 7],
+    orbital_mechanics: OrbitalMechanics,
     current_planet: String,
     selected_planet: String,
     current_year: String,
@@ -33,11 +31,12 @@ struct MyApp {
 }
 
 impl MyApp {
-    pub fn new(planets: [Planet; 7]) -> Self {
-        let current_planet = "Kerbin".to_string();
-        let selected_planet = "Duna".to_string();
+    pub fn new() -> Self {
+        let orbital_mechanics = OrbitalMechanics::new();
+        let current_planet = orbital_mechanics.current_planet.clone();
+        let selected_planet = orbital_mechanics.selected_planet.clone();
         Self {
-            planets,
+            orbital_mechanics,
             current_planet,
             selected_planet,
             current_year: "0".to_string(),
@@ -48,6 +47,12 @@ impl MyApp {
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &Context, frame: &mut Frame) {
+        self.orbital_mechanics.current_planet = self.current_planet.clone();
+        self.orbital_mechanics.selected_planet = self.selected_planet.clone();
+        match self.orbital_mechanics.set_time(&self.current_year, &self.current_day) {
+            Ok(_) => (),
+            Err(_e) => () // How should this be handled?
+        };
         egui::CentralPanel::default().show(ctx, |ui| {
             // TODO: Set the spacing here - ui.set_style() <- this needs some style struct
             ui.heading("Orbital Window Calculator");
@@ -56,17 +61,17 @@ impl eframe::App for MyApp {
                 egui::ComboBox::from_label("Select current planet")
                     .selected_text(format!("{}", self.current_planet))
                     .show_ui(ui, |ui| {
-                        for planet in &self.planets {
+                        for planet in &self.orbital_mechanics.planets {
                             ui.selectable_value(&mut self.current_planet, planet.name.clone(), &planet.name);
                         }
                     });
             });
             ui.horizontal(|ui| {
-                let name_label = ui.label("Selected Planet: ");
+                ui.label("Selected Planet: ");
                 egui::ComboBox::from_label("Select target planet")
                     .selected_text(format!("{}", self.selected_planet))
                     .show_ui(ui, |ui| {
-                        for planet in &self.planets {
+                        for planet in &self.orbital_mechanics.planets {
                             ui.selectable_value(&mut self.selected_planet, planet.name.clone(), &planet.name);
                         }
                     });
@@ -94,16 +99,16 @@ impl eframe::App for MyApp {
                     ui.label("Days until next transfer window begins");
                     ui.label("Days until optimal launch");
                     ui.end_row();
-                    for planet in &self.planets {
+                    for planet in &self.orbital_mechanics.planets {
                         if planet.name == self.selected_planet {
                             ui.label(RichText::new(format!("{}", planet.name)).color(Color32::RED));
                         }
                         else {
                             ui.label(format!("{}", planet.name));
                         }
-                        let transfer_window = Planet::get_orbital_period(&self.planets, &self.current_planet, &planet.name, TimeUnit::Seconds);
-                        ui.label(format!("{:.1}", Planet::convert_time_from_seconds(transfer_window, TimeUnit::Days)));
-                        let next_transfer_window = Planet::get_next_transfer_window(&self.current_year, &self.current_day, transfer_window, TimeUnit::Days);
+                        let transfer_window = self.orbital_mechanics.get_orbital_period(&planet, TimeUnit::Seconds);
+                        let next_transfer_window = self.orbital_mechanics.get_next_transfer_window(transfer_window, TimeUnit::Days);
+                        ui.label(format!("{:.1}", OrbitalMechanics::convert_time_from_seconds(transfer_window, TimeUnit::Days)));
                         ui.label(format!("{:.1}", next_transfer_window));
                         ui.label("TODO");
                         ui.end_row();
--- src/planet.rs
+++ src/planet.rs
@@ -1,5 +1,78 @@
+use std::num::ParseFloatError;
+
 const SECONDS_IN_YEAR: u32 = 9203544;
-const SECONDS_IN_DAY: u32 = 21600;
+const SECONDS_IN_DAY: u32 = 21600; // A Kerbin day is 6 hours long, not 24
+
+pub struct OrbitalMechanics {
+    pub planets: [Planet; 7],
+    pub current_planet: String,
+    pub selected_planet: String,
+    current_year: f32,
+    current_day: f32,
+    total_time_passed: f32
+}
+
+impl OrbitalMechanics {
+    pub fn new() -> Self {
+        Self {
+            planets: Planet::get_all_planets(),
+            current_planet: "Kerbin".to_string(),
+            selected_planet: "Duna".to_string(),
+            current_year: 0f32,
+            current_day: 0f32,
+            total_time_passed: 0f32
+        }
+    }
+
+    pub fn set_time(&mut self, current_year: &str, current_day: &str) -> Result<(), ParseFloatError> {
+        let year = match current_year.parse::<f32>() {
+            Ok(t) => t,
+            Err(e) => return Err(e)
+        };
+        let day = match current_day.parse::<f32>() {
+            Ok(t) => t,
+            Err(e) => return Err(e)
+        };
+        let year_seconds = year * SECONDS_IN_YEAR as f32;
+        let day_seconds = day * SECONDS_IN_DAY as f32;
+        self.current_year = year;
+        self.current_day = day;
+        self.total_time_passed = year_seconds + day_seconds;
+        Ok(())
+    }
+
+    pub fn get_orbital_period(&self, target_planet: &Planet, time_unit: TimeUnit) -> f32 {
+        if self.current_planet == target_planet.name {
+            return 0 as f32;
+        }
+        let mut starting_orbital_period: u32 = 0;
+        let mut target_orbital_period: u32 = 0;
+        for planet in &self.planets {
+            if planet.name == self.current_planet {
+                starting_orbital_period = planet.orbital_period;
+            }
+            if planet.name == target_planet.name {
+                target_orbital_period = planet.orbital_period;
+            }
+        }
+        // abs( 1 / ( (1/orbital_period_start) - (1/orbital_period_end) ) )
+        let res: f32 = 1f32 / ( (1f32 / starting_orbital_period as f32) - (1f32 / target_orbital_period as f32) );
+        let absolute = res.abs();
+        Self::convert_time_from_seconds(absolute, time_unit)
+    }
+
+    pub fn get_next_transfer_window(&self, transfer_window: f32, time_unit: TimeUnit) -> f32 {
+        let time_into_current_window = self.total_time_passed % transfer_window;
+        Self::convert_time_from_seconds(transfer_window - time_into_current_window, time_unit)
+    }
+
+    pub fn convert_time_from_seconds(res: f32, time_unit: TimeUnit) -> f32 {
+        match time_unit {
+            TimeUnit::Seconds => res,
+            TimeUnit::Days => res / SECONDS_IN_DAY as f32
+        }
+    }
+}
 
 pub struct Planet {
     pub name: String,
@@ -29,46 +102,5 @@ impl Planet {
             Self::new("Jool".to_string(), 104661432),
             Self::new("Eeloo".to_string(), 156992048),
         ]
-    }
-
-    pub fn get_orbital_period(planets: &[Planet; 7], starting_planet: &str, ending_planet: &str, time_unit: TimeUnit) -> f32 {
-        if starting_planet == ending_planet {
-            return 0 as f32;
-        }
-        let mut starting_orbital_period: u32 = 0;
-        let mut target_orbital_period: u32 = 0;
-        for planet in planets {
-            if planet.name == starting_planet {
-                starting_orbital_period = planet.orbital_period;
-            }
-            if planet.name == ending_planet {
-                target_orbital_period = planet.orbital_period;
-            }
-        }
-        // abs( 1 / ( (1/orbital_period_start) - (1/orbital_period_end) ) )
-        let res: f32 = 1f32 / ( (1f32 / starting_orbital_period as f32) - (1f32 / target_orbital_period as f32) );
-        let absolute = res.abs();
-        Self::convert_time_from_seconds(absolute, time_unit)
-    }
-
-    pub fn get_next_transfer_window(current_year: &str, current_day: &str, transfer_window: f32, time_unit: TimeUnit) -> f32 {
-        let year = match current_year.parse::<f32>() {
-            Ok(t) => t * SECONDS_IN_YEAR as f32, // Convert years to seconds. Kerbins orbital period is the length of a KSP year
-            Err(_e) => return -1f32
-        };
-        let day = match current_day.parse::<f32>() {
-            Ok(t) => t * SECONDS_IN_DAY as f32, // Convert days to seconds
-            Err(_e) => return -1f32
-        };
-        let total_time_passed = year + day;
-        let time_into_current_window = total_time_passed % transfer_window;
-        Self::convert_time_from_seconds(transfer_window - time_into_current_window, time_unit)
-    }
-
-    pub fn convert_time_from_seconds(res: f32, time_unit: TimeUnit) -> f32 {
-        match time_unit {
-            TimeUnit::Seconds => res,
-            TimeUnit::Days => res / SECONDS_IN_DAY as f32
-        }
     }
 }
